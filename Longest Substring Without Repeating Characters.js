9dcd00e3e7a1a0a7e8df2f15d377ada24c73fed5
Amneh-Hammami <62937263+Amneh-Hammami@users.noreply.github.com>
2021-06-24 13:36:41 +0300

    Update Longest Substring Without Repeating Characters.js
    succeed to find the number of characters in the string without repeating characters but still not finding the longest substring.

--- Longest Substring Without Repeating Characters.js
+++ Longest Substring Without Repeating Characters.js
@@ -9,23 +9,43 @@ Explanation: The answer is "abc", with the length of 3.
 
 var lengthOfLongestSubstring = function(s) {
     var arr = s.split("");
-    var array = [];
-    var count1 = 0;
-    var count2 = 0;
-    var start;
-    var end;
-
     for(var i=0; i<arr.length; i++){
-        start = arr[i];
-        end = arr[i+1];
-            if(start !== end){
-                count1 += 1;
-                array.push(start);
-            }else if(start === end){
-                count2 += 1; 
+        var test = arr[i];
+        for(var j=i+1; j<arr.length; j++){
+            if(test === arr[j]){
+                arr.splice(j,1);
             }
+        }
     }
-   
+ 
+    for(var b=0; b<arr.length; b++){
+            var test2 = arr[b];
+            for(var c=b+1; c<arr.length; c++){
+                if(test2 === arr[c]){
+                    arr.splice(c,1);
+                }
+               
+            }
+        }
+      return (arr.length);
     }
 
-  
+    // var count = 0;
+    // var s2 = s.split("");
+    // for(var l=s2.length-1; l>=0; l--){
+    //     var start = s2[l];
+    //     count += 1; 
+    //     for(var i= 0; i<s2.length; i++){
+    //         if(start === s2[i]){
+    //             s2.splice(i,1)
+    //         }
+    //         if (s2.splice(i,1)){
+    //             for(var i= 0; i<s2.length; i++){
+    //               if(start === s2[i]){
+    //                s2.splice(i,1)
+    //              }
+    //             }
+    //         }
+    //     }
+    // }
+    // return count;
